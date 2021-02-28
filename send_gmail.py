f3527064426018b5d98da64b514fe713fe40e22f
yahata1311 <yahata1311@gmail.com>
2021-03-01 00:51:59 +0900

    pycharmからメールを送信してみた。相手先１件Ver

--- send_gmail.py
+++ send_gmail.py
@@ -9,14 +9,14 @@ def main():
     mail_key = os.environ['mail_key']  # gmailのPWをpycharmの環境変数に入れた。
     receive_add = os.environ['receive_add']  # 相手先のﾒｱﾄﾞをpycharmの環境変数に入れた。
 
-    # 以下にGmailの設定を書き込む★ --- (*1)
+    # Gmailの設定を書き込む
     gmail_account = gmail_address
     gmail_password = mail_key
 
-    # メールの送信先★ --- (*2)
+    # メールの送信先
     mail_to = receive_add
 
-    # メールデータ(MIME)の作成 --- (*3)
+    # メールデータ(MIME)の作成
     subject = "メール送信テスト"
     body = "pycharmからメール送信テストです。<br>一斉送信できるようになりたい。" \
            "<br>まずは宛先１件バージョン"
@@ -25,7 +25,7 @@ def main():
     msg["To"] = mail_to
     msg["From"] = gmail_account
 
-    # Gmailに接続 --- (*4)
+    # Gmailに接続
     server = smtplib.SMTP_SSL("smtp.gmail.com", 465,
                               context=ssl.create_default_context())
     server.login(gmail_account, gmail_password)
