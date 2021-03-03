bbb8fdb86e81d9e61a892d08d8ca839f0dfd89e2
yahata1311 <yahata1311@gmail.com>
2021-03-03 19:16:00 +0900

    添付ファイルをおくってみよう

--- send_mail_order.py
+++ send_mail_order.py
@@ -1,25 +1,30 @@
+# 添付ファイルを送ってみよう
+
 import codecs
 import datetime  # 日付ﾓｼﾞｭｰﾙ
 import smtplib  # メール送信ﾓｼﾞｭｰﾙ
-import ssl
+import ssl  # SSL
 import sys
+from email import encoders  # 添付ファイルをメールで送るで送る為に変換ﾓｼﾞｭｰﾙ
 from email.mime.text import MIMEText  # 日本語送信ﾓｼﾞｭｰﾙ
 
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout)  # エンコード指定
 
-gmail_account = "test@example.com"
-gmail_password = パスワード
-mail_to = "test@example.com"
+gmail_account = "yahata1311@email.com"
+gmail_password = "yuki7777"
+mail_to = "y.yahata0128@gmail.com"
 name = "矢羽田"
 
 # 納期を自動でメールに記載したい時
-today_date = datetime.date.today()
-delivery_date = today_date + datetime.timedelta(days=7)
+today_date = datetime.date.today()  # 日付
+delivery_date = today_date + datetime.timedelta(days=7)  # 納期７日後
 print(today_date, delivery_date)
 
 # 件名
-subject = "{0}様、発注書の送付について".format(name)
-body = "“{0}の発注書をお送りいたします。".format(date)
+subject = "{0}様、{1}分の発注書のお送りします。".format(sand_name, today_date)
+body = "表題の発注書をお送りいたします。,<br>添付ファイルをご確認下さい。" \
+       "<br>本発注の納期は{0}となります。" \
+       "<br><br>株式会社○○".format(delivery_date)
 
 import pandas as pd
 
@@ -47,15 +52,21 @@ def gmail_send(send_name, mail_to, filename):
     msg_body = MIMEText(body, "html")
     msg.attach(msg_body)
 
+    # 添付ファイルを追加する（PDF）
     filename = "order_a.pdf"
     file = open(filename, "rb")
+
+    # 添付ファイルの指定
     attachment_file = MIMEBase('application', 'pdf')
     attachment_file.set_payload((file).read())
     file.close()
+
+    # 添付ファイルを添付させる
     encoders.encode_base64(attachment_file)
     attachment_file.add_header('Content - Disposition', "attachment", filename=filename)
     msg.attach(attachment_file)
 
+    # SMTPサーバー
     server = smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context())
     server.login(gmail_account, gmail_password)
     server.send_message(msg)
