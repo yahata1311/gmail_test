618820eea6a816e5185f38ae6aa9778e53de5925
yahata1311 <yahata1311@gmail.com>
2021-03-08 00:44:44 +0900

    CSVファイルの宛先一覧へ一斉送信。テスト済み

--- send_mail_few.py
+++ send_mail_few.py
@@ -35,47 +35,43 @@ df
 # print()
 
 for send_name, mail_to, filename in zip(df['宛名'], df['メールアドレス'], df['添付ファイル']):
-    print(send_name, mail_to, filename)
+    # print(send_name, mail_to, filename)
 
+    def gmail_send(send_name, mail_to, filename):
+        subject = "資料を送付させて頂きます。".format(send_name, to_date)
 
-def gmail_send(send_name, mail_to, filename):
-    subject = "資料を送付させて頂きます。".format(send_name, to_date)
-
-    body = '''いつもお世話になっております。<br>
+        body = '''いつもお世話になっております。<br>
             表題の資料をお送りします。,<br>
             ご確認お願い致します。<br><br>
 
             株式会社○○'''.format(delivery_date)
 
-    msg = MIMEMulipart()
+        msg = MIMEMulipart()
 
-    msg['Subject'] = subject
-    msg['TO'] = mail_to
-    msg['From'] = gmail_account
-    msg_body = MIMEText(body, "html")
-    msg.attach(msg_body)
+        msg['Subject'] = subject
+        msg['TO'] = mail_to
+        msg['From'] = gmail_account
+        msg_body = MIMEText(body, "html")
+        msg.attach(msg_body)
 
-    # 添付ファイルを追加する（PDF）
-    filename = "order_a.pdf"
-    file = open(filename, "rb")
+        # 添付ファイルを追加する（PDF）
+        filename = "order_a.pdf"
+        file = open(filename, "rb")
 
-    # 添付ファイルの指定
-    attachment_file = MIMEBase('application', 'pdf')
-    attachment_file.set_payload((file).read())
-    file.close()
+        # 添付ファイルの指定
+        attachment_file = MIMEBase('application', 'pdf')
+        attachment_file.set_payload((file).read())
+        file.close()
 
-    # 添付ファイルを添付させる
-    encoders.encode_base64(attachment_file)
-    attachment_file.add_header('Content - Disposition', "attachment", filename=filename)
-    msg.attach(attachment_file)
+        # 添付ファイルを添付させる
+        encoders.encode_base64(attachment_file)
+        attachment_file.add_header('Content - Disposition', "attachment", filename=filename)
+        msg.attach(attachment_file)
 
-    # SMTPサーバー
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context())
-    server.login(gmail_account, gmail_password)
-    server.send_message(msg)
-    sarver.close()
-    print(send_name, '様:送信完了')
+        # SMTPサーバー
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context())
+        server.login(gmail_account, gmail_password)
+        server.send_message(msg)
+        sarver.close()
 
-
-if __name__ == '__gmail_send__':
-    main()
+        print('OK')
